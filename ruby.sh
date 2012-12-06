02aadb5b9e8f7552aada019999ff0d2c99d481b6
Svilen Vassilev <svilen@rubystudio.net>
2012-12-06 14:18:24 +0200

    Bump ruby to patchlevel 327

--- ruby.sh
+++ ruby.sh
@@ -16,9 +16,9 @@ git config --global user.email $input_email
 git config --global user.name "$input_first_name $input_last_name"
 
 # Download and install Ruby 1.9.2
-wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p194.tar.gz
-tar xfvz ruby-1.9.3-p194.tar.gz
-cd ruby-1.9.3-p194
+wget http://ftp.ruby-lang.org/pub/ruby/1.9/ruby-1.9.3-p327.tar.gz
+tar xfvz ruby-1.9.3-p327.tar.gz
+cd ruby-1.9.3-p327.tar.gz
 ./configure
 sudo make
 sudo make install
